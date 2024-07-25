defd33f4469a02c2c15a60bb3791494e59bf3cd2
KatrinTrayers <katariinasein@gmail.com>
2024-07-25 14:10:02 +0300

    add active remove active
    make script work

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 
 $(document).ready(function(){  //make stuff when document is ready
+    $(".burgermenue").click(function(){ //specific object selected element do something click
+        $(".menupanel").addClass("active");
+    });
     $(".menupanel__closing").click(function(){ //specific object selected element do something click
-        console.log("jewerly");
+        $(this).parents(".menupanel").removeClass("active");//finds the element to be clicked and finds its parents with class menupanel removes class active 
+
     });
   });
